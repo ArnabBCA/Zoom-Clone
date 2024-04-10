7369414c6f512ffd0dd902e16d5b6b9c9d45841a
Arnab Ghosh <arnabsonu2@gmail.com>
2024-04-10 21:27:37 +0530

    init and init sidebar

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="flex sixe-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Home</h1>
+    </section>
+  );
+};
+
+export default Home;
