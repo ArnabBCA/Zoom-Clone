febcb8bc7ea1b9af2700f8b68123a15702dea4e0
Arnab Ghosh <arnabsonu2@gmail.com>
2024-04-12 12:33:47 +0530

    added navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src={"/icons/logo.svg"}
+          width={40}
+          height={40}
+          alt="Zoom Logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Zoom
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        <MobileNav/>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,10 +12,10 @@ const Sidebar = () => {
   const pathName = usePathname();
   return (
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
           const isActive =
-            pathName === link.route || pathName.startsWith(link.route);
+            pathName === link.route || pathName.startsWith(`${link.route}/`);
           return (
             <Link
               key={link.label}
