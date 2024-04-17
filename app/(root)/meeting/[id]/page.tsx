641e48660c4c908d4d1d5580cdb64e86a0405735
Arnab Ghosh <arnabsonu2@gmail.com>
2024-04-17 13:00:10 +0530

    Added Meeting Setup

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -19,7 +19,11 @@ const Meeting = ({ params: { id } }: { params: { id: string } }) => {
     <main className="h-screen w-full ">
       <StreamCall call={call}>
         <StreamTheme>
-          {!isSetipComplete ? <MeetingSetup /> : <MeetingRoom />}
+          {!isSetipComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
         </StreamTheme>
       </StreamCall>
     </main>
