6a70520d98b7a8f9ab01b38ddd8492b09d6de11b
Arnab Ghosh <arnabsonu2@gmail.com>
2024-04-20 23:24:59 +0530

    Update Zoom logo text to uppercase in MobileNav component

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -36,7 +36,7 @@ const MobileNav = () => {
               alt="Zoom Logo"
               className="max-sm:size-10"
             />
-            <p className="text-[26px] font-extrabold text-white">Zoom</p>
+            <p className="text-[26px] font-extrabold text-white">ZOOM</p>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
